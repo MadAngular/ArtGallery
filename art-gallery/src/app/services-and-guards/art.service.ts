db5797c5b5190537ec96418583f6c0aa3aafc99f
Aleksandar Aleksandrov <70431835+TheStormWeaver@users.noreply.github.com>
2021-08-08 14:03:17 +0300

    Add files via upload

--- art-gallery/src/app/services-and-guards/art.service.ts
+++ art-gallery/src/app/services-and-guards/art.service.ts
@@ -1,25 +1,27 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore, CollectionReference } from '@angular/fire/firestore';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { Art } from '../models/Art';
-import { Like } from '../models/Like';
-import { AuthService } from './auth.service';
-import { v4 as uuidv4 } from 'uuid';
+import { Injectable } from "@angular/core";
+import { AngularFirestore, CollectionReference } from "@angular/fire/firestore";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { Art } from "../models/Art";
+import { Like } from "../models/Like";
+import { AuthService } from "./auth.service";
+import { v4 as uuidv4 } from "uuid";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ArtService {
   ArtCollection: CollectionReference;
   LikesCollection: CollectionReference;
   collLen = 0;
-  constructor(private firestore: AngularFirestore,
+  constructor(
+    private firestore: AngularFirestore,
     public authService: AuthService,
     private router: Router,
-    private toastr: ToastrService) {
-    this.ArtCollection = firestore.collection<Art>('art').ref;
-    this.LikesCollection = firestore.collection<Like>('likes').ref;
+    private toastr: ToastrService
+  ) {
+    this.ArtCollection = firestore.collection<Art>("art").ref;
+    this.LikesCollection = firestore.collection<Like>("likes").ref;
   }
 
   getArt() {
@@ -37,19 +39,19 @@ export class ArtService {
       description: description,
       imageURL: imageUrl,
       likes: 0,
-    }
+    };
     let id = uuidv4();
-    return this.ArtCollection.doc(id).set(art)
+    return this.ArtCollection.doc(id).set(art);
   }
 
   editArt(art: any, artId: string) {
-    return this.ArtCollection.doc(artId)
-      .set(art);
+    return this.ArtCollection.doc(artId).set(art);
   }
 
   removeArtById(id: string) {
     this.toastr.warning("Deleting art piece...");
-    this.ArtCollection.doc(id).delete()
+    this.ArtCollection.doc(id)
+      .delete()
       .then((data) => {
         this.toastr.success("Art piece deleted!");
         this.router.navigate(["art"]);
